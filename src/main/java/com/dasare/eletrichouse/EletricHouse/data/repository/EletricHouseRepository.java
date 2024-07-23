fef4d277b5c86310898aa05ada56feb75790e4fb
Renee Barros <reneebarros4@gmail.com>
2024-07-22 23:10:48 -0300

    Modificado-pacotes_Criado classe EltrichouseEntity, criado EletricHouseRepository.

---
+++ src/main/java/com/dasare/eletrichouse/EletricHouse/data/repository/EletricHouseRepository.java
@@ -0,0 +1,7 @@
+package com.dasare.eletrichouse.EletricHouse.data.repository;
+
+import com.dasare.eletrichouse.EletricHouse.model.EletricHouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EletricHouseRepository extends JpaRepository<EletricHouse, Long> {
+}
